418256498a639f727c7d948a383ec9d333e948ae
Joshua Partogi <joshua.java@gmail.com>
2009-09-10 23:32:06 +1000

    initial implementation of login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :mains
+  map.resources :logins
 
   # The priority is based upon order of creation: first created -> highest priority.
 
