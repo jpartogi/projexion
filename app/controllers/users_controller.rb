ab1bbba9a1f020ba606f00b56c8ddd9cf798fc4c
Joshua Partogi <joshua.java@gmail.com>
2009-09-16 07:32:57 +1000

    implement new style controller on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,22 @@
 class UsersController < ApplicationController
+  respond_to :html, :json
+  
   # GET /users
   # GET /users.xml
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
   end
 
   # GET /users/1
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/new
   # GET /users/new.xml
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/1/edit
@@ -76,10 +63,5 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
-    end
   end
 end
