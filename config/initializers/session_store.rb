862b78509ec2d8bc50de7d3a25ff5d9765f1c463
Joshua Partogi <joshua.java@gmail.com>
2009-10-08 00:16:21 +0000

    Updated to latest Rails3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_rails_session',
-  :secret      => 'secretsecretsecretsecretsecret' #change this
+  :key         => '_projexion_session',
+  :secret      => '58e218c52c5fd34c2e488524a3b4a54e9888257aa1e4bf39d16f1086bc91006851cdde9649e87fac7ee91e9fef08ab269465ebb1c056f8b10ea9a3c4715a4a2e'
 }
 
 # Use the database for sessions instead of the cookie-based default,
