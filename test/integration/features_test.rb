160b06fcc6327f3d24f02537a944dae4563878b1
Joshua Partogi <jpartogi@vostro.(none)>
2009-11-19 15:22:57 +1100

    don't need fixtures

--- test/integration/features_test.rb
+++ test/integration/features_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class FeaturesTest < ActionController::IntegrationTest
-  fixtures :all
+
   def test_visit_project_feature
     visit project_feature_path('PR','9999')
   end
