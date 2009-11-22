94106acb174a55226272ad80096f1c65aefda5b6
Joshua Partogi <jpartogi@vostro.(none)>
2009-11-22 16:38:59 +1100

    commented out that login route for the time being

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
 
   map.resources :user_sessions
-  map.login  '/login',  :controller => "user_sessions", :action => "new"
-  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
+  #map.login  '/login',  :controller => "user_sessions", :action => "new"
+  #map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
