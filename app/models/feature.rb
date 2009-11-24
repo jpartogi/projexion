4d8aab11871175b696a79c3f20e203517f0260ea
Joshua Partogi <jpartogi@vostro.(none)>
2009-11-25 00:01:54 +1100

    added more TODO notes here and there

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -3,6 +3,6 @@ class Feature < ActiveRecord::Base
   belongs_to :release
   belongs_to :project
   has_many :tasks
-
+  #TODO: Add accepted property to identify whether feature has been accepted by product owner
   validates_presence_of :user_story, :acceptance_test
 end
