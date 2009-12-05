938461a17148df3ed62f00f6640bb230cb4e8631
Joshua Partogi <jpartogi@r1.(none)>
2009-12-06 05:04:48 +1100

    assign next position on create

--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -1,5 +1,10 @@
 class TaskStatus < ActiveRecord::Base
   has_many :tasks
 
-  #TODO: By default, on save the position is the next largest
+  before_create :set_next_position
+  
+  def set_next_position
+    last_status = TaskStatus.find(:last, :order => "position")
+    self.position = last_status.position + 1
+  end
 end
---
+++ test/unit/task_status_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+require 'shoulda'
+
+class TaskStatusTest < ActiveSupport::TestCase
+  fixtures :all
+
+  context "A Task status instance" do
+    should "assign the next position" do
+      task_status = TaskStatus.new
+      task_status.display_name = 'Verified'
+      task_status.key = 'verified'
+      task_status.color = '#000'
+
+      assert task_status.save
+      assert_equal task_status.position, 4
+    end
+  end
+end
