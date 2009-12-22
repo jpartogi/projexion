9c904586be69784c46a560e071c4fd808bfffa89
Joshua Partogi <jpartogi@r1.(none)>
2009-12-22 21:37:10 +1100

    Add dashboard link

--- app/controllers/acceptances_controller.rb
+++ app/controllers/acceptances_controller.rb
@@ -1,5 +1,16 @@
 class AcceptanceController < ApplicationController
 
+  def destroy
+    @acceptance = Acceptance.find(params[:id])
+
+    @acceptance.destroy
+
+    respond_to do |format|
+      flash[:notice] = 'Acceptance test was successfully deleted.'
+      format.html { redirect_to project_feature_path(:code => params[:project_id], :id => params[:feature_id]) }
+    end
+  end
+
   # Ajax actions
   def update_acceptance
     @acceptance = Acceptance.find(params[:acceptance_id])
