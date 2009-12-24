e0adccc6358824ef1e979a37d24587d5fd1bed8c
Joshua Partogi <jpartogi@r1.(none)>
2009-12-24 20:46:52 +1100

    Make sure there's only one default status

--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -1,11 +1,24 @@
 class TaskStatus < ActiveRecord::Base
   has_many :tasks
 
-  before_create :set_next_position
+  before_create :check_and_update_default_status, :set_next_position
+  before_update :check_and_update_default_status
   
   def set_next_position
     last_status = TaskStatus.find(:last, :order => "position")
     self.position = last_status.position + 1
+  end
+
+  def check_and_update_default_status
+    default = TaskStatus.default_status
+
+    unless default == nil
+      if self.default_status
+        # Change the current default status to make sure there's only one default status
+        default.default_status = false
+        default.save
+      end
+    end
   end
 
   def update_position(direction)
@@ -26,5 +39,21 @@ class TaskStatus < ActiveRecord::Base
     end
 
     return self
+  end
+  #TODO: if there's already default status, change it on update
+  
+  # Class methods
+  class << self
+    def default_status
+      TaskStatus.find(:first, :conditions => [ "default_status = ?", true ] )
+    end
+    
+    def default_status_exists?
+      unless self.default_status == nil
+        true
+      else
+        false
+      end
+    end
   end
 end
