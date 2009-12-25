ea8f099d6c6113eb4f866237cce044b7fa3c9c17
Joshua Partogi <jpartogi@r1.(none)>
2009-12-25 17:17:53 +1100

    String in ruby way

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,9 +2,11 @@ class Sprint < ActiveRecord::Base
   belongs_to :project
   has_many :features
 
+  #TODO: Extract retrospective to meetings model
+
   validates_presence_of :start_date, :end_date
 
   def span_date
-    self.start_date.to_formatted_s + " - " + self.end_date.to_formatted_s
+    "#{self.start_date.to_formatted_s} - #{self.end_date.to_formatted_s}"
   end
 end
