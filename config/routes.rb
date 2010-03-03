08a476d784e3bdf9ab6ed3efd3185c7959c43bd6
Joshua <joshua.partogi@gmail.com>
2010-03-03 02:44:30 -0800

    Add user deactivate route

--- config/routes.rb
+++ config/routes.rb
@@ -10,31 +10,6 @@ Projexion::Application.routes.draw do |map|
   resources :user_sessions
   map.login  '/login',  :controller => "user_sessions", :action => "new"
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-  #map.settings 'settings', :controller => 'settings'
 
   match 'projects/:project_id/sprints/taskboard' => 'sprints#taskboard', :as => :taskboard
 
@@ -71,7 +46,12 @@ Projexion::Application.routes.draw do |map|
   end
 
   namespace :admin do  
-  	resources :users  
+  	resources :users do
+      member do
+        post :deactivate
+      end
+    end
+      
     resources :task_statuses
     resources :meeting_types  
   end
