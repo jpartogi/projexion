13eabaa03add2c0e77dc48c08c69a5bdc2192416
Joshua <joshua.partogi@gmail.com>
2010-03-03 02:43:58 -0800

    User administration views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,4 +6,52 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def show
+  	@user = User.find(params[:id])
+  	
+  	@project_members = @user.project_members
+  end
+  
+  def new
+  	@user = User.new
+  end
+  
+  def edit
+  	@user = User.find(params[:id])
+  end
+  
+  def create
+	@user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to admin_user_path(@user), :notice => 'User was successfully created.' }
+      else
+        format.html { render :action => "new" }
+      end
+    end  
+  end
+  
+  def update
+  	@user = User.find(params[:id])
+
+    respond_with(@user) do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to admin_user_path(@user), :notice => 'User was successfully updated.' }
+      else
+        format.html { render :action => "edit" }
+      end
+    end 	
+  end
+  
+  def destroy
+  	@user = User.find(params[:id])
+  	
+  	@user.destroy
+  	
+  	respond_to do |format|
+      format.html { redirect_to admin_user_path, :notice => 'User status was successfully deleted.' }
+    end	
+  end
 end 
