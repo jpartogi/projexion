9ae85ecc6b58085f2ec83ba686bb1ed1b80a3264
Joshua <joshua.partogi@gmail.com>
2010-03-05 02:08:45 -0800

    Display latest events on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,9 @@ class MainController < ApplicationController
     before_filter :require_user, :only => :index
 
     def index
-      @projects = current_user.projects 
+      @projects = current_user.projects
+
+      @events = Event.find(:all, :conditions => { :project_id => @projects })
     end
 
     def show
