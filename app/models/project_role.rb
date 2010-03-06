fc6c0ae4a192d12ed878355b71948fdcefe01ee5
Joshua <joshua.partogi@gmail.com>
2010-03-05 23:18:12 -0800

    Add dynamic title

--- app/models/project_role.rb
+++ app/models/project_role.rb
@@ -1,6 +1,6 @@
 class ProjectRole < ActiveRecord::Base
   belongs_to :project_member
-
+  #TODO: Make sure there is only one manager
   class << self
     def manager
       ProjectRole.first(:conditions => {:manager => true})
