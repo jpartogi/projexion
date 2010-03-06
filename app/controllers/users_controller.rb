864ba0b280af7439660bbe2d6384c98fb1b2bf99
Joshua <joshua.partogi@gmail.com>
2010-03-06 00:22:42 -0800

    Show user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_login(params[:id])
     render :layout => "main" 
   end
 
