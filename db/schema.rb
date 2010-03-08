3fe89b8d4ad63b725e2b0ae3fde1c59396d1de32
Joshua <joshua.partogi@gmail.com>
2010-03-08 02:06:46 -0800

    Changed to project_id

--- db/schema.rb
+++ db/schema.rb
@@ -126,12 +126,12 @@ ActiveRecord::Schema.define(:version => 20091013000428) do
   add_index "sprints", ["id", "project_id"], :name => "sprints_idx"
 
   create_table "task_dailies", :force => true do |t|
-    t.string   "project_code", :limit => 3, :null => false
-    t.integer  "total_tasks",               :null => false
-    t.date     "last_update",               :null => false
-    t.datetime "tstamp",                    :null => false
-    t.datetime "updated_at",                :null => false
-    t.datetime "created_at",                :null => false
+    t.integer  "project_id",  :null => false
+    t.integer  "total_tasks", :null => false
+    t.date     "last_update", :null => false
+    t.datetime "tstamp",      :null => false
+    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  :null => false
   end
 
   create_table "task_statuses", :force => true do |t|
