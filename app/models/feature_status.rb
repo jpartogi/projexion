218884c562dadd87e2a0b340ea425aa7c937764f
Joshua <joshua.partogi@gmail.com>
2010-03-12 23:19:32 -0800

    Fix feature status position and default

--- app/models/feature_status.rb
+++ app/models/feature_status.rb
@@ -1,11 +1,14 @@
 class FeatureStatus < ActiveRecord::Base
   has_many :features
 
-  before_create :check_and_update_default_status, :set_next_position
-  before_update :check_and_update_default_status
+  before_create :set_next_position
+  before_save :check_and_update_default_status
+
+  DIRECTION_UP = 'up'
+  DIRECTION_DOWN = 'down'
 
   def set_next_position
-    last_status = FeatureStatus.find(:last, :order => "position")
+    last_status = FeatureStatus.last(:order => "position")
 
     unless last_status.nil?
       self.position = last_status.position + 1
@@ -13,51 +16,74 @@ class FeatureStatus < ActiveRecord::Base
   end
 
   def check_and_update_default_status
-    default = FeatureStatus.default_status
-
-    unless default.nil?
-      if self.default_status and not @@bypass
-        # Change the current default status to make sure there's only one default status
-        default.default_status = false
-        default.save
-      end
+    if default_status_available? and not @bypass and self.default_status
+      update_default_status
+      self.position = 1
     end
   end
 
   def update_position(direction)
     position = self.position
 
-    if direction.eql? 'up'
-      status = FeatureStatus.find_by_position(position.to_i - 1)
-    else
-      status = FeatureStatus.find_by_position(position.to_i + 1)
+    if direction.eql? DIRECTION_UP
+      changed_status = FeatureStatus.find_by_position(position.to_i - 1)
+    elsif direction.eql? DIRECTION_DOWN
+      changed_status = FeatureStatus.find_by_position(position.to_i + 1)
     end
-    
-    self.position = status.position
-    status.position = position
 
-    @@bypass = true
+    self.position = changed_status.position
+    logger.debug "Target position: " + self.position.to_s
 
-    if self.position.eql? 1
+    changed_status.position = position
+    logger.debug "Changed position: " + changed_status.position.to_s
+
+    @bypass = true #We want to bypass the interceptor because there are two instance.save
+
+    # Let's make sure first position is always the default status
+    if self.position == 1
       self.default_status = true
-      default = FeatureStatus.default_status
-      default.default_status = false
-      default.save
+    else
+      self.default_status = false
+    end
+
+    if changed_status.position == 1
+      changed_status.default_status = true
+    else
+      changed_status.default_status = false
     end
 
     FeatureStatus.transaction do
       self.save
-      status.save
+      changed_status.save
     end
 
     return self
   end
 
+  private
+    def default_status_available?
+      @current_default = FeatureStatus.default_status
+
+      not @current_default.nil? # reverse it
+    end
+
+    def update_default_status
+      # Change the current default status to make sure there's only one default status
+      @current_default.default_status = false
+      @current_default.position = self.position
+
+      @current_default.save
+    end
+  
   # Class methods
   class << self
 
     def default_status
       FeatureStatus.first(:conditions => {:default_status => true})
+    end
+
+    def first_status
+      FeatureStatus.first(:conditions => {:position => 1})
     end
 
   end
--- test/unit/feature_status_test.rb
+++ test/unit/feature_status_test.rb
@@ -6,10 +6,32 @@ class FeatureStatusTest < ActiveSupport::TestCase
 
   context "A Task status instance" do
     should "update the position" do
-      feature_status = FeatureStatus.find(1)
+      feature_status = FeatureStatus.find(2)
 
-      assert feature_status.update_position('down')
+      assert feature_status.update_position(FeatureStatus::DIRECTION_UP)
+      assert_equal feature_status.position, 1
+      assert_equal feature_status.default_status, true
+
+      feature_status_1 = FeatureStatus.find(1)
+      assert_equal feature_status_1.position, 2
+      assert_equal feature_status_1.default_status, false
+
+      assert feature_status.update_position(FeatureStatus::DIRECTION_DOWN)
       assert_equal feature_status.position, 2
+      assert_equal feature_status.default_status, false
+    end
+
+    should "update the default status" do
+      feature_status = FeatureStatus.find(3)
+
+      feature_status.default_status = true
+      assert feature_status.save
+
+      assert_equal feature_status.default_status, true
+      assert_equal feature_status.position, 1
+
+      # Make sure there's only one default status
+      assert_equal FeatureStatus.count(:conditions => {:default_status => true}), 1
     end
   end
 end
