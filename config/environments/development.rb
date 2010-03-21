da46ea5f5fcb848df0fb146267dbe259cd7a759f
Joshua <joshua.partogi@gmail.com>
2010-03-21 04:38:17 -0700

    Ooops

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,8 @@ Projexion::Application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :user_name => 'projexionist@gmail.com',
-      :password => 'love2009',
+      :user_name => '',
+      :password => '',
       :authentication => 'plain',
       :enable_starttls_auto => true
   }
