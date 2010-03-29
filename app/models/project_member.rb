ff859e4874369635d6c0e4d0cb7173618fa71542
Joshua <jpartogi@scrum8.com>
2010-03-29 04:55:10 -0700

    Add TODOs

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -2,4 +2,6 @@ class ProjectMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :project_role
+
+  #TODO: Make sure there is only one project manager per project
 end
