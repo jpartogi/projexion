4bdc768fba2891a6fa1178d9601d3468566fca41
Joshua <jpartogi@scrum8.com>
2010-03-30 03:51:33 -0700

    Add action mailers sendmail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,15 +15,15 @@ Projexion::Application.configure do
   config.action_controller.perform_caching             = false
 
   # Don't care if the mailer can't send
-  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => '',
-      :password => '',
-      :authentication => 'plain',
-      :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :sendmail
+#  config.action_mailer.smtp_settings = {
+#      :address => "smtp.gmail.com",
+#      :port => 587,
+#      :user_name => '',
+#      :password => '',
+#      :authentication => 'plain',
+#      :enable_starttls_auto => true
+#  }
 end
