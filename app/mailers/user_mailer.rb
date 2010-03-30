af8c97948f05fc9b2b16057b57a41c3445901f81
Joshua <jpartogi@scrum8.com>
2010-03-30 03:58:07 -0700

    add TODOs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(:to => user.email,  :subject => "Welcome to Projexion Demo")
+    mail(:to => user.email, :subject => "Welcome to Projexion Demo")
   end 
 end
--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -1,5 +1,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
+    #TODO Send email in a separate thread
     UserMailer.welcome_email(user).deliver
   end
 end
