96bdf93dd6348be9bdaf032377de558eed1a272c
Joshua <jpartogi@scrum8.com>
2010-03-30 03:45:46 -0700

    Added mailer smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,11 @@ Projexion::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
   # Defaults to: # config.action_mailer.sendmail_settings = { # :location => '/usr/sbin/sendmail', # :arguments => '-i -t' # }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
