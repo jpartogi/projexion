83787b08b660f79f63969d3dd8ab2a846a088406
Joshua <jpartogi@scrum8.com>
2010-03-31 03:18:55 -0700

    Enhance welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "support@projexionist.com"
+  default :from => "Projexion Support <support@projexionist.com>"
 
   def welcome_email(user)
     @user = user
