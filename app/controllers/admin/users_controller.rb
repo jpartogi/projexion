6fb0aa3f14dfbb7f36a37a080a1f4ff9660c5e8b
Joshua <jpartogi@scrum8.com>
2010-03-31 03:20:01 -0700

    Fix typo

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -52,7 +52,7 @@ class Admin::UsersController < ApplicationController
   	@user.destroy
   	
   	respond_to do |format|
-      format.html { redirect_to admin_users_path, :notice => 'User status was successfully deleted.' }
+      format.html { redirect_to admin_users_path, :notice => 'User was successfully deleted.' }
     end	
   end
 
