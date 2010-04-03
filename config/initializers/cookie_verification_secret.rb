b5330f831e4543286eaf4ea5423bc3de7435bf2d
Joshua <jpartogi@scrum8.com>
2010-04-02 21:11:08 -0700

    Prepare for Rails 3.0.0.beta2

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
+# Rails.application.config.cookie_secret in the future Rails 3
 ActionController::Base.cookie_verifier_secret = '5cf139ba5c710685f38b6f6cbc3c054ef4da20ff6afc08f4e0412b5e77626e1266fdff34a768b1675f6cd538475b7f5e5bea208ce874f8a41b65198e25283be9';
