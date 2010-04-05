0993569ee03b32ced6f2b76d4e81e09b7e554e0a
Joshua <jpartogi@scrum8.com>
2010-04-04 19:33:38 -0700

    Check if project manager role for corresponding project already exists

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -6,7 +6,9 @@ class ProjectMember < ActiveRecord::Base
   validate :only_one_manager_per_project
 
   def only_one_manager_per_project
-    manager = ProjectMember.first(:conditions => {:project_id => self.project, :project_role_id => ProjectRole.manager})
-    errors.add(:manager, "role for #{self.project.name} already exists") unless manager.nil?
+    if self.project_role == ProjectRole.manager
+      manager = ProjectMember.first(:conditions => {:project_id => self.project, :project_role_id => self.project_role})
+      errors.add(:manager, "role for #{self.project.name} already exists") unless manager.nil?
+    end
   end
 end
