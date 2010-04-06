bc83446f1fac4c46ad09d807bc0d946a90f456d5
Joshua <jpartogi@scrum8.com>
2010-04-06 02:35:07 -0700

    Refactor project member validation

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -6,9 +6,8 @@ class ProjectMember < ActiveRecord::Base
   validate :only_one_manager_per_project
 
   def only_one_manager_per_project
-    if self.project_role == ProjectRole.manager
-      manager = ProjectMember.first(:conditions => {:project_id => self.project, :project_role_id => self.project_role})
-      errors.add(:manager, "role for #{self.project.name} already exists") unless manager.nil?
+    if self.project_role.eql? ProjectRole.manager and self.project.manager_exists?
+      errors.add(:manager, "role for #{self.project.name} already exists")
     end
   end
 end
