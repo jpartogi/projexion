e989eb9962bd2a5f21c3456918858e0d37e2fa84
Joshua <jpartogi@scrum8.com>
2010-04-20 02:47:20 -0700

    add validation when saving project member

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -3,6 +3,7 @@ class ProjectMember < ActiveRecord::Base
   belongs_to :project
   belongs_to :project_role
 
+  validates_presence_of :user, :project, :project_role
   validate :only_one_manager_per_project
 
   def only_one_manager_per_project
