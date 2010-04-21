78c9927803df993dd9b2a75a07e80e3c2ea53225
Joshua <jpartogi@scrum8.com>
2010-04-21 05:28:20 -0700

    Added security session configuration settings

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,8 @@
 class UserSession < Authlogic::Session::Base
+  consecutive_failed_logins_limit = 3
+  last_request_at_threshold = 1.minute
+  logout_on_timeout = true
+
   class << self
     def current_user
       current_user_session = UserSession.find
