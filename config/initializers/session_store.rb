9b60b68ccf7df1625f3c0ac70e5072647dcf9872
Joshua <jpartogi@scrum8.com>
2010-04-23 19:12:40 -0700

    Update config initialization with Rails 3 way

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,10 +4,7 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_projexion_session',
-  :secret      => '177af34e99017c64f06954e25a0a9e22cfa30abc5b2f78755937b6907a5eff207456bdaf7dc581b4ea33c25d03ee6d54d1c51d26d374b88c4b18657cc79e1990'
-}
+Rails.application.config.session_store :cookie_store, :key => '_projexion_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
