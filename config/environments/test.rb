ffc630837460334a42f42f3424fef6ad9ce7237c
Joshua <jpartogi@scrum8.com>
2010-08-28 23:10:15 +0000

    deprecation notice for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 Projexion::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
-
+  config.active_support.deprecation = :stderr
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
