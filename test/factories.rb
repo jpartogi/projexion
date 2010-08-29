a763911f3c93c90b60ee0dbcc5e54a5b4c011da0
Joshua <jpartogi@scrum8.com>
2010-08-29 06:38:29 +0000

    Add priority factories

--- test/factories.rb
+++ test/factories.rb
@@ -1,22 +1,23 @@
-Factory.define :projexion, :class => Project do |project|
+Factory.define :priority do |priority|
+  priority.display_name 'High'
+  priority.color 'blue'
+  priority.level 10
+end
+
+Factory.define :project do |project|
   project.name 'Projexion'
   project.code 'PR2'
   project.vision 'To be an awesome free Agile project management tool'
 end
 
-Factory.define :dummy, :class => Project do |project|
-  project.name 'dummy'
-  project.code 'dummy'
-  project.vision 'Dummy project'
-end
-
 Factory.define :release do |release|
-  
+  release.version_number '1.0.0'
+  release.project Factory.stub(:project)
 end
 
-Factory.define :sprint, :class => Sprint do |sprint|
-  sprint.start_date Date.today - 2.days
-  sprint.end_date Date.today + 12.days
+Factory.define :sprint do |sprint|
+  sprint.start_date Time.now - 2.days
+  sprint.end_date Time.now + 12.days
   sprint.release Factory.stub(:release)
-  sprint.project Factory(:projexion)
+  sprint.project Factory.stub(:project)
 end
