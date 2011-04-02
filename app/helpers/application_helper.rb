9559067374891e893b395432652e2895d13631b0
Joshua Partogi <joshua.partogi@gmail.com>
2011-04-02 19:15:45 +1100

    Combo box helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     "Cancel this operation"
   end
   #TODO: Add delete confirmation message
+
+  def options_from_collection_for_select_with_blank(collection, value_method, text_method, selected = nil)
+    raw "<option value=''></option>".concat(options_from_collection_for_select(collection, value_method, text_method, selected))
+  end
 end
